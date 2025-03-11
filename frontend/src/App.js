6c545fb5f81614801c2c076891cc76bf80f361d0
Prakhar Dhyani <prakhardhyani@Prakhars-MacBook-Pro.local>
2025-03-11 13:57:03 +0530

    added UI for CodeEditor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import CodeEditor from "./components/CodeEditor";
+import Header from "./components/Header";
+import "./App.css"
 function App() {
+  const [language, setLanguage] = useState("javascript"); 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header setLanguage={setLanguage} />
+      <CodeEditor language={language} />
     </div>
   );
 }
 
 export default App;
+
