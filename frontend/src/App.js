04861bfdaf157fb51be209948bf6ae29351d9ba7
Prakhar Dhyani <prakhardhyani@Prakhars-MacBook-Pro.local>
2025-03-13 12:52:05 +0530

    added button to send call to llm

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,39 @@
 import React, { useState } from "react";
-import { Routes, Route , useLocation} from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import CodeEditor from "./components/CodeEditor";
 import LoginPage from "./components/LoginPage";
-import "./App.css"
+import "./App.css";
 import SignUpPage from "./components/SignUpPage";
+import CodeAnalyzer from "./components/CodeAnalyzer";
 import Header from "./components/Header";
+
 function App() {
   const [language, setLanguage] = useState("javascript");
   const location = useLocation();
+  const [code, setCode] = useState("");
+
+
   return (
     <>
       {location.pathname === "/" && <Header setLanguage={setLanguage} />}
-        <Routes>
-          <Route path="/" element={<CodeEditor language={language} />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path = "/signup" element = {<SignUpPage/>}/>
-        </Routes>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="main-container">
+              <div className="editor-container">
+                <CodeEditor language={language} code={code} setCode={setCode}/>
+              </div>
+              <div className="analyzer-container">
+                <CodeAnalyzer code={code} />
+              </div>
+              
+            </div>
+          }
+        />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
     </>
   );
 }
