975d9d70732dc0e47866f7bd645b914bc979aab5
Prakhar Dhyani <prakhardhyani@Prakhars-MacBook-Pro.local>
2025-03-14 16:14:36 +0530

    formatted the response from Judge0

--- frontend/src/components/RunCode.js
+++ frontend/src/components/RunCode.js
@@ -1,18 +1,16 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState } from "react";
 import AuthContext from "../context/AuthContext";
 import "../css/RunCode.css";
 
-const RunCode = ({ code , language}) => {
+const RunCode = ({ code, language }) => {
   const { executeCode } = useContext(AuthContext);
   const [responseText, setResponseText] = useState("");
-  const [displayText, setDisplayText] = useState("");
   const [loading, setLoading] = useState(false);
 
   const handleClick = async () => {
     setLoading(true);
     setResponseText("");
-    setDisplayText("");
-  
+
     const languageMap = {
       "c++": 52,
       "python": 71,
@@ -20,19 +18,19 @@ const RunCode = ({ code , language}) => {
       "java": 62,
       "javascript": 63,
     };
-  
+
     const selectedLanguageId = languageMap[language.toLowerCase()] || null;
-  
+
     if (!selectedLanguageId) {
       setResponseText("Unsupported language.");
       setLoading(false);
       return;
     }
-  
+
     setTimeout(async () => {
       try {
         const response = await executeCode(selectedLanguageId, code);
-        const fullText = response.response || JSON.stringify(response);
+        const fullText = response.stderr || response.compileError || response.stdout || JSON.stringify(response);
         setResponseText(fullText);
       } catch (error) {
         console.error("Error calling LLM:", error);
@@ -42,20 +40,6 @@ const RunCode = ({ code , language}) => {
       }
     }, 3000);
   };
-  
-
-  useEffect(() => {
-    if (responseText) {
-      setDisplayText(""); 
-      let index = 0;
-      const interval = setInterval(() => {
-        setDisplayText((prev) => prev + responseText[index]);
-        index++;
-        if (index === responseText.length) clearInterval(interval);
-      }, 10); 
-      return () => clearInterval(interval);
-    }
-  }, [responseText]);
 
   return (
     <div className="card run-code-container">
@@ -64,16 +48,16 @@ const RunCode = ({ code , language}) => {
           <div className="spinner">
             <div className="spinnerin"></div>
           </div>
-        ) : displayText ? (
+        ) : responseText ? (
           <div className="styled-response">
-            {displayText.slice(0, -9).split("\n").map((line, index) => (
+            {responseText.split("\n").map((line, index) => (
               <p key={index} className={line.startsWith("**") ? "bold-text" : ""}>
                 {line}
               </p>
             ))}
           </div>
         ) : (
-          "Your Output will be displayed here "
+          "Your Output will be displayed here"
         )}
       </div>
       <button className="use-geass-button" onClick={handleClick} disabled={loading}>
